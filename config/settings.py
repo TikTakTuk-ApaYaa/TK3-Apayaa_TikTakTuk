148c8a398300e87a8f1f68c3a93e0e523246b597
naomi kyla <naomikyla@nomay-2.local>
2026-05-15 15:16:22 +0700

    Fix CSRF Trusted Origins for Railway

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-lokal-aja')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [
-    'web-production-13ce7.up.railway.app', 
     'web-production-e6018.up.railway.app'
     'localhost', 
     '127.0.0.1'
